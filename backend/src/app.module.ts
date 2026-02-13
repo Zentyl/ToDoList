baff41f080c47a3432b9ecbf52cc96d0ae22e953
Mateusz Szczerba <matszc32@wp.pl>
2026-02-13 12:03:51 +0100

    Splitting modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,18 +1,12 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AuthController } from './auth/auth.controller';
-import { AuthService } from './auth/auth.service';
 import { AppService } from './app.service';
-import { TaskService } from './task/task.service';
-import { TaskController } from './task/task.controller';
-import { UserService } from './user/user.service';
-import { UserController } from './user/user.controller';
 import { Task } from './task/task.entity';
 import { User } from './user/user.entity';
-import { JwtStrategy } from './auth/jwt.strategy';
+import { AuthModule } from './auth/auth.module';
+import { TaskModule } from './task/task.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -22,28 +16,15 @@ import { JwtStrategy } from './auth/jwt.strategy';
       entities: [Task, User],
       synchronize: true,
     }),
-    TypeOrmModule.forFeature([Task, User]),
-
-    PassportModule.register({ defaultStrategy: 'jwt' }),
-
-    JwtModule.register({
-      global: true,
-      secret: 'tajne_haslo',
-      signOptions: { expiresIn: '1h' },
-    }),
+    AuthModule,
+    TaskModule,
+    UserModule,
   ],
   controllers: [
     AppController,
-    AuthController,
-    TaskController,
-    UserController,
   ],
   providers: [
     AppService,
-    JwtStrategy,
-    AuthService,
-    UserService,
-    TaskService,
   ],
 })
 export class AppModule { }
---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
+import { UserModule } from '../user/user.module';
+
+@Module({
+    imports: [
+        UserModule,
+        PassportModule.register({ defaultStrategy: 'jwt' }),
+        JwtModule.register({
+            global: true,
+            secret: 'tajne_haslo',
+            signOptions: { expiresIn: '1h' },
+        }),
+    ],
+    controllers: [
+        AuthController,
+    ],
+    providers: [
+        JwtStrategy,
+        AuthService,
+    ],
+})
+export class AuthModule { }
